d13b095e33ccb7064a5b105cd05f5fbfd8bc808c
Antonio Carlos de Lima Junior <ac@marketmining.com.br>
2025-07-27 21:07:55 -0300

    feat: use variables

--- scripts/run-migrations.sh
+++ scripts/run-migrations.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Rodando migrations Flyway para BigQuery..."
+if [[ -z "$GCP_PROJECT_ID" || -z "$BIGQUERY_DATASET" || -z "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
+  echo "ERRO: Variáveis obrigatórias não definidas."
+  echo "Defina GCP_PROJECT_ID, BIGQUERY_DATASET e GOOGLE_APPLICATION_CREDENTIALS."
+  exit 1
+fi
+
+echo "Rodando migrations para o projeto: $GCP_PROJECT_ID | dataset: $BIGQUERY_DATASET"
 flyway -configFiles=conf/flyway.conf migrate
